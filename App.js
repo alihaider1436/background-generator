a8346b8e528ab357e5dd38e486975169cd6806bb
Ali Haider <aliisalihaider@gmail.com>
2021-08-27 01:28:30 +0500

    this is my first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./component/card-list/card-list-component";
+import { SearchBox } from "./component/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchFiled: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  handleChange = (e) => {
+    this.setState({ searchFiled: e.target.value });
+  };
+  render() {
+    const { monsters, searchFiled } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    );
+    return ( 
+      <div className="App">
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
